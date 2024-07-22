3d1658b3839cee3945434d21aa061c9edc78c9c0
mnaufaldillah <mnaufaldillah@gmail.com>
2024-07-22 19:23:48 +0700

    feat: Get Product By Id For Public Site

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ const router = require(`express`).Router();
 router.post(`/`, ProductController.createProduct);
 router.get(`/`, ProductController.getProducts);
 router.get(`/pub`, ProductController.getProductsPublic);
+router.get(`/pub/:productId`, ProductController.getProductByIdPublic);
 router.get(`/:productId`, ProductController.getProductById);
 router.put(`/:productId`, ProductController.updateProductById);
 router.delete(`/:productId`, ProductController.deleteProductById)
