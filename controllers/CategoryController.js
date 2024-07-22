04b1f2eaa2a3ebfe723e0546e546a2b16f72c008
mnaufaldillah <mnaufaldillah@gmail.com>
2024-07-22 18:27:02 +0700

    feat: Update Category By Id

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -28,6 +28,31 @@ class CategoryController {
             res.status(500).json({ message: `Internal Server Error` });
         }
     }
+
+    static async updateCategoryById(req, res) {
+        try {
+            const { categoryId } = req.params;
+            const { name } = req.body;
+
+            const category = await Category.findByPk(categoryId);
+
+            if (!category) {
+                res.status(404).json({ message: `Product with id ${categoryId} not found`})
+            } else {
+                await category.update({
+                    name
+                });
+
+                res.status(200).json(category);
+            }
+        } catch (error) {
+            if(error.name === `SequelizeValidationError`) {
+                res.status(400).json({ message: error.errors[0].message });
+            } else {
+                res.status(500).json({ message: `Internal Server Error` });
+            }
+        }
+    }
 }
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,6 @@ const router = require(`express`).Router();
 
 router.post(`/`, CategoryController.createCategory);
 router.get(`/`, CategoryController.getCategories);
+router.put(`/:categoryId`, CategoryController.updateCategoryById);
 
 module.exports = router;
