ac1eb796d8de63ac8f49fa96a00ba9fe5b5740dc
mnaufaldillah <mnaufaldillah@gmail.com>
2024-07-22 18:20:10 +0700

    feat: Get Categories List

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -18,6 +18,16 @@ class CategoryController {
             }
         }
     }
+
+    static async getCategories(req, res) {
+        try {
+            const categories = await Category.findAll();
+
+            res.status(200).json(categories);
+        } catch (error) {
+            res.status(500).json({ message: `Internal Server Error` });
+        }
+    }
 }
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -2,5 +2,6 @@ const CategoryController = require("../controllers/CategoryController");
 const router = require(`express`).Router();
 
 router.post(`/`, CategoryController.createCategory);
+router.get(`/`, CategoryController.getCategories);
 
 module.exports = router;
